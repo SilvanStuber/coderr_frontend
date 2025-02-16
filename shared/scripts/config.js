b04521549de633c9e854dadca5389c3ce82aa51c
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2025-02-16 15:12:46 +0100

    adaptation of the imprint and the routing

--- shared/scripts/config.js
+++ shared/scripts/config.js
@@ -9,8 +9,8 @@ const GUEST_LOGINS = {
   },
 };
 
-const API_BASE_URL = "http://127.0.0.1:8000/api/";
-const STATIC_BASE_URL = "http://127.0.0.1:8000/";
+const API_BASE_URL = "https://gc.silvanstuber.ch/api/";
+const STATIC_BASE_URL = "https://gc.silvanstuber.ch";
 
 const LOGIN_URL = "login/";
 
